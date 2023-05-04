d2447d30aebd0629696f860cf7c9dbd49bb1bb58
Joyce Melo <joycee.mello@hotmail.com>
2023-05-03 21:53:09 -0300

    teste

--- app.js
+++ app.js
@@ -1,24 +1,43 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Container } from '@material-ui/core';
-import Navbar from './Navbar';
-import HomePage from './HomePage';
-import SobrePage from './SobrePage';
-import ContatoPage from './ContatoPage';
+<select id="dados-dropdown" onchange="console.log(event.target.value)">
+  <option value="">Selecione um nome</option>
+</select>
 
-function App() {
-  return (
-    <Router>
-      <Navbar />
-      <Container>
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route exact path="/sobre" component={SobrePage} />
-          <Route exact path="/contato" component={ContatoPage} />
-        </Switch>
-      </Container>
-    </Router>
-  );
+
+<form onsubmit="handleSubmit(event)">
+  <input type="text" value={nome} onchange="nome = event.target.value" />
+  <button type="submit">Salvar</button>
+</form>
+
+
+
+let nome = '';
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const dados = JSON.parse(localStorage.getItem('dados')) || [];
+  const novoDado = {
+    nome: nome,
+    valor: nome.toLowerCase().replace(/\s+/g, '-'), // opcional: transforma o nome em uma string URL-friendly
+  };
+  const novosDados = [...dados, novoDado];
+  localStorage.setItem('dados', JSON.stringify(novosDados));
+  nome = '';
+  atualizarDropdown();
 }
 
-export default App;
+function atualizarDropdown() {
+  const dados = JSON.parse(localStorage.getItem('dados')) || [];
+  const dropdown = document.getElementById('dados-dropdown');
+  dropdown.innerHTML = '';
+  const defaultOption = document.createElement('option');
+  defaultOption.text = 'Selecione um nome';
+  defaultOption.value = '';
+  dropdown.add(defaultOption);
+  for (let i = 0; i < dados.length; i++) {
+    const option = document.createElement('option');
+    option.text = dados[i].nome;
+    option.value = dados[i].valor;
+    dropdown.add(option);
+  }
+}
+
